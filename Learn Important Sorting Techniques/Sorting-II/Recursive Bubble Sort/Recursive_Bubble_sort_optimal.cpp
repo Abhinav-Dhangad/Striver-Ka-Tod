f4bb5c0745fbae08fafcdd17ad89adf08a47709f
Abhinav Dhangad <abhinavdhangad10219xa.sja@gmail.com>
2026-07-21 16:11:27 +0530

    Add files via upload

---
+++ Learn Important Sorting Techniques/Sorting-II/Recursive Bubble Sort/Recursive_Bubble_sort_optimal.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void Bubble_Sort(int arr[],int n){
+    if(n==1) return;
+    int swaps=0;
+    for(int j=0;j<n-2;j++){
+        if(arr[j]>arr[j+1]){
+            int temp=arr[j+1];
+            arr[j+1]=arr[j];
+            arr[j]=temp;
+            swaps++;
+        }
+    }
+    if(swaps==0){
+        return;
+    }
+    Bubble_Sort(arr,n-1);
+}
+
+void Print_array(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter the noumber of elements : ";
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"Enter element "<<i+1<<" : ";
+        cin>>arr[i];
+    }
+
+    Bubble_Sort(arr,n);
+    Print_array(arr,n);
+}
