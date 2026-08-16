704e6bb1a6596d922f450c2088bba4b7008a2cdb
Abhinav Dhangad <abhinavdhangad10219xa.sja@gmail.com>
2026-08-16 22:38:52 +0530

    Add files via upload

---
+++ Arrays/Medium/Q_2_Count_012_optimal.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+void sortnums(vector<int> &nums){
+    int low=0;
+    int mid=0;
+    int high=nums.size()-1;
+
+    while(mid<=high){
+        if(nums[mid]==0){
+            swap(nums[low],nums[mid]);
+            low++;
+            mid++;
+        }
+        else if(nums[mid]==1){
+            mid++;
+        }
+        else{
+            swap(nums[mid],nums[high]);
+            high--;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size of the array : ";
+    cin>>n;
+
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cout<<"Enter element "<<i+1<<" : ";
+        cin>>arr[i];        
+    }
+
+    sortnums(arr);
+    cout<<"The sorted array is :-"<<endl;
+    for(auto i : arr){
+        cout<<i<<" ";
+    } 
+}
