f63e20d259c05538ff398099fe354d57dee6dfa8
Abhinav Dhangad <abhinavdhangad10219xa.sja@gmail.com>
2026-08-19 15:40:59 +0530

    Refactor Kadane's algorithm implementation

--- Arrays/Medium/Q_4_Kandane_Algorithm/Q_4_Kandane_Algorithm_optimal.cpp
+++ Arrays/Medium/Q_4_Kandane_Algorithm/Q_4_Kandane_Algorithm_optimal.cpp
@@ -1,53 +1,31 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 
 using namespace std;
 //An actually good approach which can be used in numberous questions.
-vector<int> maxSubArray(vector<int> &nums)
-{
-    int maxi = INT_MIN;
-    int sum = 0;
+int maxSubArray(vector<int>& nums){
+    int maxsum=nums[0];
+    int sum=nums[0];
 
-    int start = 0;
-    int ansStart = -1, ansEnd = -1;
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if (sum == 0)
-        {
-            start = i;
-        }
-        sum += nums[i];
-        if (sum > maxi)
-        {
-            maxi = sum;
-            ansStart = start;
-            ansEnd = i;
-        }
-
-        if (sum < 0)
-        {
-            sum = 0;
-        }
+    for(int i=1;i<nums.size();i++){
+        sum=max(nums[i],sum+nums[i]);
+        maxsum=max(maxsum,sum);
     }
-    return {ansStart, ansEnd, maxi};
+    return maxsum;
 }
 
-int main()
-{
+int main(){
     int n;
-    cout << "Enter the size of the arra : ";
-    cin >> n;
+    cout<<"Enter the size of the array : ";
+    cin>>n;
 
     vector<int> arr(n);
+
     for(int i=0;i<n;i++){
-        cout<<"Enter the element "<<i+1<<" : ";
-        cin>>arr[i];    
+        cout<<"Enter element "<<i+1<<" : ";
+        cin>>arr[i];
     }
 
-    vector<int> max_subarray = maxSubArray(arr);
-    cout<<"The subarray is : ";
-    for(int i=max_subarray[0];i<=max_subarray[1];i++){
-        cout<<arr[i]<<" ";  
-    }
-    cout<<"\nThe maximum sum of contiguous subarray is : "<<max_subarray[2]<<endl;
+    int max_sum=maxSubArray(arr);
+    cout<<"Maximum sum of contiguous subarray is : "<<max_sum<<endl;
 }
