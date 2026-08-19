d610d6ca759b75d29bf49f0dbc4b3bd6f24e7c90
Abhinav Dhangad <abhinavdhangad10219xa.sja@gmail.com>
2026-08-19 14:58:42 +0530

    Add files via upload

---
+++ Arrays/Medium/Q_4_Kandane_Algorithm/Q_4_Kandane_Algorithm_optimal.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int maxSubArray(vector<int>& nums){
+    int maxsum=nums[0];
+    int sum=nums[0];
+
+    for(int i=1;i<nums.size();i++){
+        sum=max(nums[i],sum+nums[i]);
+        maxsum=max(maxsum,sum);
+    }
+    return maxsum;
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size of the array : ";
+    cin>>n;
+
+    vector<int> arr(n);
+
+    for(int i=0;i<n;i++){
+        cout<<"Enter element "<<i+1<<" : ";
+        cin>>arr[i];
+    }
+
+    int max_sum=maxSubArray(arr);
+    cout<<"Maximum sum of contiguous subarray is : "<<max_sum<<endl;
+}
