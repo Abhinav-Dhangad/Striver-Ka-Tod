a3243b9530106493e93b9f8deaba33cb10e6eb4c
Abhinav Dhangad <abhinavdhangad10219xa.sja@gmail.com>
2026-08-19 15:39:41 +0530

    Update Kadane's Algorithm implementation
    Refactored maxSubArray to return indices and max sum.

--- Arrays/Medium/Q_4_Kandane_Algorithm/Q_4_Kandane_Algorithm_optimal.cpp
+++ Arrays/Medium/Q_4_Kandane_Algorithm/Q_4_Kandane_Algorithm_optimal.cpp
@@ -1,31 +1,53 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 //An actually good approach which can be used in numberous questions.
-int maxSubArray(vector<int>& nums){
-    int maxsum=nums[0];
-    int sum=nums[0];
+vector<int> maxSubArray(vector<int> &nums)
+{
+    int maxi = INT_MIN;
+    int sum = 0;
 
-    for(int i=1;i<nums.size();i++){
-        sum=max(nums[i],sum+nums[i]);
-        maxsum=max(maxsum,sum);
+    int start = 0;
+    int ansStart = -1, ansEnd = -1;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (sum == 0)
+        {
+            start = i;
+        }
+        sum += nums[i];
+        if (sum > maxi)
+        {
+            maxi = sum;
+            ansStart = start;
+            ansEnd = i;
+        }
+
+        if (sum < 0)
+        {
+            sum = 0;
+        }
     }
-    return maxsum;
+    return {ansStart, ansEnd, maxi};
 }
 
-int main(){
+int main()
+{
     int n;
-    cout<<"Enter the size of the array : ";
-    cin>>n;
+    cout << "Enter the size of the arra : ";
+    cin >> n;
 
     vector<int> arr(n);
-
     for(int i=0;i<n;i++){
-        cout<<"Enter element "<<i+1<<" : ";
-        cin>>arr[i];
+        cout<<"Enter the element "<<i+1<<" : ";
+        cin>>arr[i];    
     }
 
-    int max_sum=maxSubArray(arr);
-    cout<<"Maximum sum of contiguous subarray is : "<<max_sum<<endl;
+    vector<int> max_subarray = maxSubArray(arr);
+    cout<<"The subarray is : ";
+    for(int i=max_subarray[0];i<=max_subarray[1];i++){
+        cout<<arr[i]<<" ";  
+    }
+    cout<<"\nThe maximum sum of contiguous subarray is : "<<max_subarray[2]<<endl;
 }
