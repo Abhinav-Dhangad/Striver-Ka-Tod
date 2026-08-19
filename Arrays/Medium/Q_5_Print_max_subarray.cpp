fbace381bafdec60ee0e72cc5d665b84a0d7fb07
Abhinav Dhangad <abhinavdhangad10219xa.sja@gmail.com>
2026-08-19 15:43:46 +0530

    Update Q_5_Print_max_subarray.cpp

--- Arrays/Medium/Q_5_Print_max_subarray.cpp
+++ Arrays/Medium/Q_5_Print_max_subarray.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
-vector<int> maxSubArray(vector<int> &nums)
+vector<long long> maxSubArray(vector<int> &nums)
 {
     long long maxi = LLONG_MIN;
     long long sum = 0;
 
     int start = 0;
     int ansStart = -1, ansEnd = -1;
+
     for (int i = 0; i < nums.size(); i++)
     {
         if (sum == 0)
         {
             start = i;
         }
+
         sum += nums[i];
+
         if (sum > maxi)
         {
             maxi = sum;
@@ -29,25 +33,35 @@ vector<int> maxSubArray(vector<int> &nums)
             sum = 0;
         }
     }
-    return {ansStart, ansEnd};
+
+    return {ansStart, ansEnd, maxi};
 }
 
 int main()
 {
     int n;
-    cout << "Enter the size of the arra : ";
+    cout << "Enter the size of the array: ";
     cin >> n;
 
     vector<int> arr(n);
-    for(int i=0;i<n;i++){
-        cout<<"Enter the element "<<i+1<<" : ";
-        cin>>arr[i];    
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> arr[i];
     }
 
-    vector<int> max_subarray = maxSubArray(arr);
-    cout << "The maximum sum of the subarray is: " << max_subarray[1] - max_subarray[0] + 1 << endl;
-    cout<<"The subarray is : ";
-    for(int i=max_subarray[0];i<=max_subarray[1];i++){
-        cout<<arr[i]<<" ";  
+    vector<long long> max_subarray = maxSubArray(arr);
+
+    cout << "The subarray is: ";
+
+    for (int i = max_subarray[0]; i <= max_subarray[1]; i++)
+    {
+        cout << arr[i] << " ";
     }
+
+    cout << "\nThe maximum sum of contiguous subarray is: "
+         << max_subarray[2] << endl;
+
+    return 0;
 }
