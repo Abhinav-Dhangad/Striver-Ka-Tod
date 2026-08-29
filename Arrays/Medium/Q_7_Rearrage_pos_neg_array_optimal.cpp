b669e8f2c896ab06fff054c476507cf674db89d4
Abhinav Dhangad <abhinavdhangad10219xa.sja@gmail.com>
2026-08-29 16:36:19 +0530

    Add files via upload

---
+++ Arrays/Medium/Q_7_Rearrage_pos_neg_array_optimal.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> rearrangeArray(vector<int> &nums)
+{
+    vector<int> return_nums(nums.size());
+    int pos = 0;
+    int neg = 1;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] > 0)
+        {
+            return_nums[pos] = nums[i];
+            pos += 2;
+        }
+        else
+        {
+            return_nums[neg] = nums[i];
+            neg += 2;
+        }
+    }
+    return return_nums;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    vector<int> rearranged_array = rearrangeArray(arr);
+
+    cout << "The rearranged array is: ";
+    for (int i = 0; i < rearranged_array.size(); i++)
+    {
+        cout << rearranged_array[i] << " ";
+    }
+    cout << endl;
+}
