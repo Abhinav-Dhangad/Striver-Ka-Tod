b3511c3c386dd1e85fcf5ca5f08620b5deaa5f66
Abhinav Dhangad <abhinavdhangad10219xa.sja@gmail.com>
2026-09-03 23:55:57 +0530

    Add files via upload

---
+++ Arrays/Medium/Q_11_Set_Matrix_Zero_good.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+void setZeroes(vector<vector<int>> &matrix)
+{
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    vector<int> row(m, 0);
+    vector<int> col(n, 0);
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (row[i] == 1 || col[j] == 1)
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int m, n;
+    cout << "Enter the number of rows : ";
+    cin >> m;
+    cout << "Enter the number of columns : ";
+    cin >> n;
+
+    vector<vector<int>> matrix(m, vector<int>(n));
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "Enter element at position (" << i + 1 << "," << j + 1 << ") : ";
+            cin >> matrix[i][j];
+        }
+    }
+
+    setZeroes(matrix);
+
+    cout << "The modified matrix is :-" << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
