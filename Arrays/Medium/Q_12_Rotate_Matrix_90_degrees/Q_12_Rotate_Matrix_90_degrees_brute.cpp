99f9b3da15818789e842a9caaf74e8b3cdf7accd
Abhinav Dhangad <abhinavdhangad10219xa.sja@gmail.com>
2026-09-06 23:32:37 +0530

    Add files via upload

---
+++ Arrays/Medium/Q_12_Rotate_Matrix_90_degrees/Q_12_Rotate_Matrix_90_degrees_brute.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> rotateClockwise(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    vector<vector<int>> rotated(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            rotated[j][n - i - 1] = matrix[i][j];
+        }
+    }
+    return rotated;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the matrix : ";
+    cin >> n;
+
+    vector<vector<int>> matrix(n, vector<int>(n));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "Enter element at position (" << i + 1 << "," << j + 1 << ") : ";
+            cin >> matrix[i][j];
+        }
+    }
+
+    matrix = rotateClockwise(matrix);
+
+    cout << "The rotated matrix is :-" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
